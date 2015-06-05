b7a11f409ff574f01f6294a56c58f6d1c5e71da7
Vladimir Bataev <artbataev@Vladimirs-MacBook-Pro.local>
2015-06-05 18:25:16 +0300

    Доделан футер для главной страницы

--- js/script.js
+++ js/script.js
@@ -5,91 +5,92 @@
   }
 
   var form = document.querySelector(".js-contest-form");
-  var allFiles = [];
+  if (form) {
+    var allFiles = [];
 
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
 
-    var data = new FormData(form);
-    allFiles.forEach(function(element) {
-      data.append("images", element.file);
+      var data = new FormData(form);
+      allFiles.forEach(function(element) {
+        data.append("images", element.file);
+      });
+      console.log(data);
+      
+      request(data, function(response) {
+        console.log(response);
+      });
+
     });
-    console.log(data);
-    
-    request(data, function(response) {
-      console.log(response);
-    });
 
-  });
-
-  function request(data, fn) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://simonenko.su/academy/echo?" + (new Date()).getTime());
-    xhr.addEventListener("readystatechange", function() {
-      if (xhr.readyState == 4) {
-        fn(xhr.responseText);
-      }
-    });
-    xhr.send(data);
-  }
-
-  if ("FileReader" in window) {
-    var area = document.querySelector(".js-upload-images");
-
-    form.querySelector("#upload-photos").addEventListener("change", function() {
-        var files = this.files;
-        for (var i = 0; i < files.length; i++) {
-          preview(files[i]);
+    function request(data, fn) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://simonenko.su/academy/echo?" + (new Date()).getTime());
+      xhr.addEventListener("readystatechange", function() {
+        if (xhr.readyState == 4) {
+          fn(xhr.responseText);
         }
-        this.value = "";
-    });
+      });
+      xhr.send(data);
+    }
 
-    function preview(file) {
-      if (file.type.match(/image.*/)) {
-        var reader = new FileReader();
+    if ("FileReader" in window) {
+      var area = document.querySelector(".js-upload-images");
 
-        reader.addEventListener("load", function(event) {
-          var container = document.createElement("li");
-          container.classList.add("user-photo");
+      form.querySelector("#upload-photos").addEventListener("change", function() {
+          var files = this.files;
+          for (var i = 0; i < files.length; i++) {
+            preview(files[i]);
+          }
+          this.value = "";
+      });
 
-          var img = document.createElement("img");
-          img.src = event.target.result; 
-          img.alt = file.name;
+      function preview(file) {
+        if (file.type.match(/image.*/)) {
+          var reader = new FileReader();
 
-          allFiles.push({file: file, img: img});
+          reader.addEventListener("load", function(event) {
+            var container = document.createElement("li");
+            container.classList.add("user-photo");
 
-          var subscription = document.createElement("span");
-          subscription.innerText = file.name;
+            var img = document.createElement("img");
+            img.src = event.target.result; 
+            img.alt = file.name;
 
-          var deleteLink = document.createElement("a");
-          deleteLink.innerText = "Удалить";
-          deleteLink.classList.add("js-delete-image");
-          deleteLink.addEventListener("click", function(event) {
-            event.preventDefault();
-            this.parentNode.remove();
+            allFiles.push({file: file, img: img});
+
+            var subscription = document.createElement("span");
+            subscription.innerText = file.name;
+
+            var deleteLink = document.createElement("a");
+            deleteLink.innerText = "Удалить";
+            deleteLink.classList.add("js-delete-image");
+            deleteLink.addEventListener("click", function(event) {
+              event.preventDefault();
+              this.parentNode.remove();
+            });
+
+            container.appendChild(img);
+            container.appendChild(subscription);
+            container.appendChild(deleteLink);
+
+            area.appendChild(container);
           });
 
-          container.appendChild(img);
-          container.appendChild(subscription);
-          container.appendChild(deleteLink);
+          reader.readAsDataURL(file);
+        }
+      } 
+    }
 
-          area.appendChild(container);
-        });
-
-        reader.readAsDataURL(file);
-      }
-    } 
+    deleteLinks = document.querySelectorAll(".js-delete-image");
+    for (var i = 0; i < deleteLinks.length; i++) {
+      var link = deleteLinks[i];
+      link.addEventListener("click", function(event) {
+        event.preventDefault();
+        this.parentNode.remove();
+      });
+    }
   }
-
-  deleteLinks = document.querySelectorAll(".js-delete-image");
-  for (var i = 0; i < deleteLinks.length; i++) {
-    var link = deleteLinks[i];
-    link.addEventListener("click", function(event) {
-      event.preventDefault();
-      this.parentNode.remove();
-    });
-  }
-
 })();
 
 
@@ -97,58 +98,88 @@
 // можно вписать "2", можно вписать "2 чел", можно вписать "2 клевых чела" - работать будет
 (function() {
   var incrementElements = document.querySelectorAll(".increment-field");
-  for (var i = 0; i < incrementElements.length; i++) {
-    initIncrementField(incrementElements[i]);
-  }
-
-  function initIncrementField(parentBlock) {
-    var input = parentBlock.querySelector("input");
-    var minus = parentBlock.querySelector(".increment-field__minus");
-    var plus = parentBlock.querySelector(".increment-field__plus");
-
-    if(minus) {
-      minus.addEventListener("click", function(){
-        changeNumber(false);
-      });
-    }
-    if(plus) {
-      plus.addEventListener("click", function(){
-        changeNumber(true);
-      });
+  if(incrementElements){
+    for (var i = 0; i < incrementElements.length; i++) {
+      initIncrementField(incrementElements[i]);
     }
 
-    input.addEventListener("keydown", function(event) {
-      var key = event.keyCode;
-      console.log(event.keyCode);
-      if(key==38) { changeNumber(true); } // стрелка вверх
-      if(key==40) { changeNumber(false); } // стрелка вниз
-    });
-  
-    function changeNumber(operation) {
-      var initText = input.value;
-      var initNumber = parseInt(initText, 10);
-      var textPosition = 0;
+    function initIncrementField(parentBlock) {
+      var input = parentBlock.querySelector("input");
+      var minus = parentBlock.querySelector(".increment-field__minus");
+      var plus = parentBlock.querySelector(".increment-field__plus");
 
-      while(textPosition < initText.length && !isNaN(parseFloat(initText[textPosition]))) {
-        textPosition++;
+      if(minus) {
+        minus.addEventListener("click", function(){
+          changeNumber(false);
+        });
       }
-      if(isNaN(initNumber) || initNumber < 0) {
-        input.value = 0;
-      } 
-      else {
-        if(operation) {
-          initNumber++;
+      if(plus) {
+        plus.addEventListener("click", function(){
+          changeNumber(true);
+        });
+      }
+
+      input.addEventListener("keydown", function(event) {
+        var key = event.keyCode;
+        console.log(event.keyCode);
+        if(key==38) { changeNumber(true); } // стрелка вверх
+        if(key==40) { changeNumber(false); } // стрелка вниз
+      });
+    
+      function changeNumber(operation) {
+        var initText = input.value;
+        var initNumber = parseInt(initText, 10);
+        var textPosition = 0;
+
+        while(textPosition < initText.length && !isNaN(parseFloat(initText[textPosition]))) {
+          textPosition++;
         }
+        if(isNaN(initNumber) || initNumber < 0) {
+          input.value = 0;
+        } 
         else {
-          initNumber = Math.max(0, initNumber - 1);
+          if(operation) {
+            initNumber++;
+          }
+          else {
+            initNumber = Math.max(0, initNumber - 1);
+          }
+          input.value =  initNumber + initText.substr(textPosition);
         }
-        input.value =  initNumber + initText.substr(textPosition);
       }
-    }
 
+    }
   }
 })();
 
+
+(function() {
+  var position = [59.938794, 30.323083];
+  function showGoogleMaps() {  
+      var latLng = new google.maps.LatLng(position[0], position[1]);
+   
+      var mapOptions = {
+          zoom: 16, // initialize zoom level - the max value is 21
+          streetViewControl: false, // hide the yellow Street View pegman
+          scaleControl: true, // allow users to zoom the Google Map
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          center: latLng
+      };
+   
+      map = new google.maps.Map(document.getElementById('index-map'),
+          mapOptions);
+   
+      // Show the default red marker at the location
+      marker = new google.maps.Marker({
+          position: latLng,
+          map: map,
+          draggable: false,
+          animation: google.maps.Animation.DROP
+      });
+  }
+   
+  google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+})();
 
 
 
